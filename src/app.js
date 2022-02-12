e9a1d2799b67a07ae2a21edb706df4f37e0e05d2
willdodsonn <sodlax38@gmail.com>
2022-02-12 00:06:28 +0000

    halfway finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").classList.add(generateRandomNumber());
+};
+
+let generateRandomNumber = function() {
+  let indexNumbers = [
+    "A",
+    "1",
+    "2",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNum = Math.floor(Math.random() * indexNumbers.length);
+  return indexNumbers[randomNum];
+};
+
+let generateRandomSuit = function() {
+  let suit = ["diamonds", "spades", "hearts", "clubs"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
 };
